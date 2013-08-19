007d707f6ff196580f6255d068d97b973cca87e9
AmitDebadwar <amit.debadwar@gmail.com>
2013-08-19 22:45:56 +0530

    s

---
+++ HtmlPro/JScript1.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="Scripts/knockout-2.2.1.js" />
+/// <reference path="Scripts/jquery-1.4.1.js" />
+
+
+function ViewModel() {
+    var self = this;
+    self.firstName = ko.observable('sai');
+}
+
+
+$(function () {
+    ko.applyBindings(new ViewModel());
+});
